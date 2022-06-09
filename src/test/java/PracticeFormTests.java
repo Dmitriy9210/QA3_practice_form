2ec017448a0b192d094ae842708aadaddfa0b862
d.avramenko <d.avramenko@prequelapp.com>
2022-06-09 10:28:06 +0700

    added new files

---
+++ src/test/java/PracticeFormTests.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class PracticeFormTests {
+
+
+    @Test
+    public void aa(){
+        open("/text-box");
+    }
+
+}
