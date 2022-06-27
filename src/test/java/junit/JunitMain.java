080ee1e1dafbc2982965f37b596eeed6e7e6204c
d.avramenko <d.avramenko@prequelapp.com>
2022-06-27 11:37:35 +0700

    update JUnit test

--- src/test/java/junit/JunitMain.java
+++ src/test/java/junit/JunitMain.java
@@ -4,9 +4,7 @@ import org.junit.jupiter.api.*;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class JunitMain {
 //    public static void main(String[] args) throws Exception {
@@ -52,40 +50,139 @@ public class JunitMain {
 //        }
 //    }
 
-    public static void main(String[] args) {
-        // Находит классы с тестами для AnnotationsExampleTest
-        // todo вынести в массив классов для проверки
-        //todo вернуться и разобрать
-        Arrays.stream(JunitTest.class.getDeclaredMethods())
-                .filter(method ->  method.getAnnotation(BeforeAll.class) != null)
-                .forEach(methodBeforeAll -> {
-                    methodBeforeAll.setAccessible(true);
-                    runMethodByClass(methodBeforeAll, JunitTest.class);
-                });
+    public static void main(String[] args) throws Exception {
+        // Находит классы с тестами
+        Method[] declaredMethods = JunitTest.class.getDeclaredMethods();
+        Method beforeAllMethod = null;
+        Method beforeEachMethod = null;
+        Method afterAllMethod = null;
+        Method afterEachMethod = null;
+        Method testAnnotations = null;
 
-        List<Method> declaredBeforeEachMethods = Arrays.stream(JunitTest.class.getDeclaredMethods())
-                .filter(method -> method.getAnnotation(BeforeEach.class) != null)
-                .collect(Collectors.toList());
-        List<Method> declaredAfterEachMethods = Arrays.stream(JunitTest.class.getDeclaredMethods())
-                .filter(method -> method.getAnnotation(AfterEach.class) != null)
-                .collect(Collectors.toList());
+        for (Method method: declaredMethods) {
+            method.setAccessible(true);
+            BeforeAll beforeAll = method.getAnnotation(BeforeAll.class);
+            BeforeEach beforeEach = method.getAnnotation(BeforeEach.class);
+            AfterEach afterEach = method.getAnnotation(AfterEach.class);
+            AfterAll afterAll = method.getAnnotation(AfterAll.class);
+            Test test = method.getAnnotation(Test.class);
+            if (beforeAll != null) {
+                beforeAllMethod = method;
+            }
+            if (beforeEach != null) {
+                beforeEachMethod = method;
+            }
+            if (afterAll != null) {
+                afterAllMethod = method;
+            }
+            if (afterEach != null) {
+                afterEachMethod = method;
+            }
+            if (test != null) {
+                testAnnotations = method;
+            }
+        }
 
-        Arrays.stream(JunitTest.class.getDeclaredMethods())
-                .filter(method -> (method.getAnnotation(Test.class) != null && method.getAnnotation(Disabled.class) == null))
-                .forEach(methodTest -> {
-                    methodTest.setAccessible(true);
-                    runMethodsByClass(declaredBeforeEachMethods, JunitTest.class);
-                    runMethodByClass(methodTest, JunitTest.class);
-                    runMethodsByClass(declaredAfterEachMethods, JunitTest.class);
-                });
+        if (beforeAllMethod != null && testAnnotations != null) {
+            try {
+                beforeAllMethod.invoke(JunitTest.class.getDeclaredConstructor().newInstance());
+            } catch (InvocationTargetException e) {
+                // Если тест упал, сообщает нам
+                System.out.println("BeforeAll упал: ");
+                throw e;
+            }
+        }
 
-        Arrays.stream(JunitTest.class.getDeclaredMethods())
-                .filter(method -> method.getAnnotation(AfterAll.class) != null)
-                .forEach(methodAfterAll -> {
-                    methodAfterAll.setAccessible(true);
-                    runMethodByClass(methodAfterAll, JunitTest.class);
-                });
+        for (Method method: declaredMethods) {
+            method.setAccessible(true);
+            // Смотрит есть ли над методом @Test
+            Test testAnnotation = method.getAnnotation(Test.class);
+            Disabled disabled = method.getAnnotation(Disabled.class);
+
+            if (testAnnotation == null) {
+                continue;
+            }
+
+            if (beforeEachMethod != null) {
+                try {
+                    beforeEachMethod.invoke(JunitTest.class.getDeclaredConstructor().newInstance());
+                } catch (InvocationTargetException e) {
+                    // Если тест упал, сообщает нам
+                    System.out.println("BeforeEach упал: ");
+                    throw e;
+                }
+            }
+
+            if (disabled == null) {
+                // Запускает
+                try {
+                    method.invoke(JunitTest.class.getDeclaredConstructor().newInstance());
+                } catch (InvocationTargetException e) {
+                    // Если тест упал, сообщает нам
+                    System.out.println("Тест упал: ");
+                    throw e;
+                }
+            }
+
+            if (afterEachMethod != null) {
+                try {
+                    afterEachMethod.invoke(JunitTest.class.getDeclaredConstructor().newInstance());
+                } catch (InvocationTargetException e) {
+                    // Если тест упал, сообщает нам
+                    System.out.println("AfterEach упал: ");
+                    throw e;
+                }
+            }
+
+        }
+
+        if (afterAllMethod != null && testAnnotations != null) {
+            try {
+                afterAllMethod.invoke(JunitTest.class.getDeclaredConstructor().newInstance());
+            } catch (InvocationTargetException e) {
+                // Если тест упал, сообщает нам
+                System.out.println("AfterAll упал: ");
+                throw e;
+            }
+        }
     }
+    // Если не упал, то все ок
+
+
+//    public static void main(String[] args) {
+//        // Находит классы с тестами для AnnotationsExampleTest
+//        // todo вынести в массив классов для проверки
+//        //todo вернуться и разобрать
+//        Arrays.stream(JunitTest.class.getDeclaredMethods())
+//                .filter(method -> method.getAnnotation(BeforeAll.class) != null)
+//                .forEach(methodBeforeAll -> {
+//                    methodBeforeAll.setAccessible(true);
+//                    runMethodByClass(methodBeforeAll, JunitTest.class);
+//                });
+//
+//        List<Method> declaredBeforeEachMethods = Arrays.stream(JunitTest.class.getDeclaredMethods())
+//                .filter(method -> method.getAnnotation(BeforeEach.class) != null)
+//                .collect(Collectors.toList());
+//        List<Method> declaredAfterEachMethods = Arrays.stream(JunitTest.class.getDeclaredMethods())
+//                .filter(method -> method.getAnnotation(AfterEach.class) != null)
+//                .collect(Collectors.toList());
+//
+//        Arrays.stream(JunitTest.class.getDeclaredMethods())
+//                .filter(method -> (method.getAnnotation(Test.class) != null && method.getAnnotation(Disabled.class) == null))
+//                .forEach(methodTest -> {
+//                    methodTest.setAccessible(true);
+//                    runMethodsByClass(declaredBeforeEachMethods, JunitTest.class);
+//                    runMethodByClass(methodTest, JunitTest.class);
+//                    runMethodsByClass(declaredAfterEachMethods, JunitTest.class);
+//                });
+//
+//        Arrays.stream(JunitTest.class.getDeclaredMethods())
+//                .filter(method -> method.getAnnotation(AfterAll.class) != null)
+//                .forEach(methodAfterAll -> {
+//                    methodAfterAll.setAccessible(true);
+//                    runMethodByClass(methodAfterAll, JunitTest.class);
+//                });
+//    }
 
     private static void runMethodByClass(Method method, Class<?> clazz) {
         try {
@@ -96,7 +193,7 @@ public class JunitMain {
     }
 
     private static void runMethodsByClass(List<Method> methodsToRun, Class<?> clazz) {
-        for (Method methodToRun : methodsToRun) {
+        for (Method methodToRun: methodsToRun) {
             methodToRun.setAccessible(true);
             runMethodByClass(methodToRun, clazz);
         }
